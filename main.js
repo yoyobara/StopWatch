f59de20561cabff2fa344662281854ef7eaea086
yoyobara <yotambarak6@gmail.com>
2020-12-29 23:33:12 +0200

    deleted unwanted branch

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-var currentSecs = 0;
-var currentMins = 0;
-var currentHours = 0;
+let currentSecs = 0;
+let currentMins = 0;
+let currentHours = 0;
+
+let counting = false;
+let interval;
+
+function updateScreen() {
+    document.getElementById("hours").innerHTML = currentHours.toString();
+    document.getElementById("minutes").innerHTML = currentMins.toString().padStart(2, '0');
+    document.getElementById("seconds").innerHTML = currentSecs.toString().padStart(2, '0');
+}
 
 function updateCounter() {
     if (currentSecs == 59) {
@@ -14,10 +23,44 @@ function updateCounter() {
     } else {
         currentSecs++;
     }
+    updateScreen();
 }
 
-function updateScreen() {
-    document.getElementById("hours").innerHTML = currentHours;
-    document.getElementById("minutes").innerHTML = currentMins;
-    document.getElementById("seconds").innerHTML = currentSecs;
+function start() {
+    //starts counting
+    interval = setInterval(updateCounter, 1000);
+    counting = true;
+    document.getElementById("startstop").innerHTML = "STOP";
+    updateScreen();
+
 }
+
+function stop() {
+    // stops counting
+    clearInterval(interval);
+    counting = false;
+    document.getElementById("startstop").innerHTML = "START";
+    updateScreen();
+
+}
+
+function reset() {
+    // resetting the count
+    clearInterval(interval);
+    counting = false;
+
+    currentHours = 0;
+    currentMins = 0;
+    currentSecs = 0;
+
+    document.getElementById("startstop").innerHTML = "START"
+
+    updateScreen();
+}
+
+document.getElementById("startstop").addEventListener("click", () => {
+    if (counting) stop();
+    else start();
+});
+
+document.getElementById("reset").addEventListener("click", reset);
