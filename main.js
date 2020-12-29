22567794cfd54aa0bedab17b0729311b7618df8c
yoyobara <yotambarak6@gmail.com>
2020-12-29 21:35:29 +0200

    fix to the updateCounter function

---
+++ main.js
@@ -0,0 +1,23 @@
+var currentSecs = 0;
+var currentMins = 0;
+var currentHours = 0;
+
+function updateCounter() {
+    if (currentSecs == 59) {
+        if (currentMins == 59) {
+            currentHours++;
+            currentMins = 0;
+        } else {
+            currentMins++;
+        }
+        currentSecs = 0
+    } else {
+        currentSecs++;
+    }
+}
+
+function updateScreen() {
+    document.getElementById("hours").innerHTML = currentHours;
+    document.getElementById("minutes").innerHTML = currentMins;
+    document.getElementById("seconds").innerHTML = currentSecs;
+}
